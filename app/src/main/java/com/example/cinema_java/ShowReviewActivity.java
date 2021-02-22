adc3abbdf6d32beaea536b3fa942330f9adef95f
devlee77 <tjdcks0928@naver.com>
2021-02-22 18:52:30 +0900

    manifest : add two activity show_review xml complete strings.xml : add show_review.xml strings

---
+++ app/src/main/java/com/example/cinema_java/ShowReviewActivity.java
@@ -0,0 +1,14 @@
+package com.example.cinema_java;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_review);
+    }
+}
