0261bfba34d68dfd38f516803e0523b5f81b1219
devlee77 <tjdcks0928@naver.com>
2021-02-22 22:26:21 +0900

    rename ShowReviewActivity -> AllRevuewActivity add button id

--- app/src/main/java/com/example/cinema_java/AllReviewActivity.java
+++ app/src/main/java/com/example/cinema_java/AllReviewActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class ShowReviewActivity extends AppCompatActivity {
+public class AllReviewActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_review);
+        setContentView(R.layout.activity_all_review);
     }
 }
